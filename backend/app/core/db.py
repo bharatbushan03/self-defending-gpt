e54c8750a8e55f4807dbfa9c42ad54ffea00dc5c
bharat03 <bharatbushan5320@gmail.com>
2026-05-04 15:03:35 +0530

    added correct mongodb connection

--- backend/app/core/db.py
+++ backend/app/core/db.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient
 
-MONGO_URI = "mongodb://localhost:27017"
+MONGO_URI = "mongodb+srv://bharatbushan03:mrBh%40r%40t12@cluster0.rwiyvjz.mongodb.net/?appName=Cluster0"
 
 client = MongoClient(MONGO_URI)
 
